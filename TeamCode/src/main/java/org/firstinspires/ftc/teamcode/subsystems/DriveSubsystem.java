a8acbfbcd59a784d78b95ddffc889bca2bae28c4
Zhe-Wei, Lee <142453567+ZheWeiLee2009@users.noreply.github.com>
2026-02-20 02:41:13 -0600

    removed recurring dependencies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -5,12 +5,10 @@ import androidx.annotation.NonNull;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import org.firstinspires.ftc.teamcode.RobotHardware;
 
 import java.util.List;
 
 public class DriveSubsystem {
-   RobotHardware bot;
 
    public double[] calculateMotorPowers(double axial, double lateral, double yaw) {
         double[] motorPowers = new double[4];
@@ -34,14 +32,13 @@ public class DriveSubsystem {
         motorPowers[3] = (rotY + rotX - yaw);
 
         return motorPowers;
-
    }
 
-    public void setMotorPowers(double v, double v1, double v2, double v3, double speedMultiplier) {
-        bot.leftFrontDrive.setPower(v * speedMultiplier);
-        bot.leftBackDrive.setPower(v1 * speedMultiplier);
-        bot.rightFrontDrive.setPower(v2 * speedMultiplier);
-        bot.rightBackDrive.setPower(v3 * speedMultiplier);
+    public void setDriveMotorPowers(@NonNull List<DcMotorEx> driveMotors, double[] velocity, double speedMultiplier) {
+        driveMotors.get(0).setPower(velocity[0] * speedMultiplier); // Front left
+        driveMotors.get(1).setPower(velocity[1] * speedMultiplier); // Back left
+        driveMotors.get(2).setPower(velocity[2] * speedMultiplier); // Front Right
+        driveMotors.get(3).setPower(velocity[3] * speedMultiplier); // Back Right
     }
 
     public void setMotorsMode(@NonNull DcMotorEx motor, DcMotorEx.RunMode mode) {
@@ -57,6 +54,7 @@ public class DriveSubsystem {
     public void setZeroPowerBehavior(@NonNull DcMotorEx motor, DcMotor.ZeroPowerBehavior zpb) {
         motor.setZeroPowerBehavior(zpb);
     }
+
     public void setZeroPowerBehavior(@NonNull List<DcMotorEx> type, DcMotor.ZeroPowerBehavior zpb) {
         for (DcMotorEx motor : type) {
             motor.setZeroPowerBehavior(zpb);
