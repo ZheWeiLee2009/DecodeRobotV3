e57d563756d88fbbc626f965a65dceb63f26dfe3
Zhe-Wei, Lee <142453567+ZheWeiLee2009@users.noreply.github.com>
2026-02-20 17:36:29 -0600

    added Intake Subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class IntakeSubsystem {
+
+    public double getIntakePower(@NonNull DcMotorEx motor) {return motor.getPower();}
+
+    /**
+     * Set power of the intake
+     *
+     * @param motor DcMotorEx, to add intake motor in here.
+     * @param lvl Low to High, 0 => 4. 0 being off, 4 being maxPower
+     */
+    public void setIntakeLevel(@NonNull DcMotorEx motor, int lvl) {
+        switch (lvl) {
+            case 0:
+                motor.setPower(0.0);
+                break;
+            case 1:
+                motor.setPower(0.4);
+                break;
+            case 2:
+                motor.setPower(0.6);
+                break;
+            case 3:
+                motor.setPower(0.8);
+                break;
+            case 4:
+                motor.setPower(1.0);
+                break;
+        }
+    }
+}
