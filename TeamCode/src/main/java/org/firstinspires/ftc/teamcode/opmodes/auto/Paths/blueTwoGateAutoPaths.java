e04caafab4423df267087243d71cb2fb9a159213
Zhe-Wei, Lee <142453567+ZheWeiLee2009@users.noreply.github.com>
2026-03-02 04:18:05 -0600

    blue side auto test 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Paths/blueTwoGateAutoPaths.java
@@ -0,0 +1,147 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto.Paths;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+
+public class blueTwoGateAutoPaths {
+    public PathChain preload;
+    public PathChain line1;
+    public PathChain gateOpen1;
+    public PathChain shoot1;
+    public PathChain align2;
+    public PathChain line2;
+    public PathChain gateOpen2;
+    public PathChain shoot2;
+    public PathChain align3;
+    public PathChain line3;
+    public PathChain shoot3;
+    public PathChain leave;
+
+
+    public blueTwoGateAutoPaths(Follower follower) {
+        preload = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(23.919, 128.233),
+
+                                new Pose(56.000, 85.000)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(144), Math.toRadians(130))
+                .setTimeoutConstraint(300)
+                .build();
+
+        line1 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(56.000, 85.000),
+
+                                new Pose(14.800, 83.924)
+                        )
+                ).setTangentHeadingInterpolation()
+
+                .build();
+
+        gateOpen1 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(14.800, 83.924),
+                                new Pose(28.955, 81.245),
+                                new Pose(12.573, 72.605)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(90))
+
+                .build();
+
+        shoot1 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(12.573, 72.605),
+
+                                new Pose(56.000, 85.000)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(134))
+
+                .build();
+
+        align2 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(56.000, 85.000),
+
+                                new Pose(45.200, 59.680)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(134), Math.toRadians(180))
+
+                .build();
+
+        line2 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(45.200, 59.680),
+
+                                new Pose(12.968, 59.680)
+                        )
+                ).setTangentHeadingInterpolation()
+
+                .build();
+
+        gateOpen2 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(12.968, 59.680),
+                                new Pose(45.860, 62.599),
+                                new Pose(12.573, 72.605)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(90))
+
+                .build();
+
+        shoot2 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(12.573, 72.605),
+
+                                new Pose(56.000, 85.000)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(134))
+
+                .build();
+
+        align3 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(56.000, 85.000),
+
+                                new Pose(45.200, 35.000)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(134), Math.toRadians(180))
+
+                .build();
+
+        line3 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(45.200, 35.000),
+
+                                new Pose(12.968, 35.000)
+                        )
+                ).setTangentHeadingInterpolation()
+
+                .build();
+
+        shoot3 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(12.968, 35.000),
+
+                                new Pose(56.000, 85.000)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(134))
+
+                .build();
+
+        leave = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(56.000, 85.000),
+
+                                new Pose(18.000, 85.000)
+                        )
+                ).setTangentHeadingInterpolation()
+
+                .build();
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/blueTwoGateAuto.java
@@ -0,0 +1,189 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.tuning.roboConstants.LauncherConstants.autoHalfShootingVelocity;
+import static org.firstinspires.ftc.teamcode.tuning.roboConstants.TransferConstants.allReleaseTimer;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.RobotHardware;
+import org.firstinspires.ftc.teamcode.opmodes.auto.Paths.blueTwoGateAutoPaths;
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.LauncherSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.TransferSubsystem;
+import org.firstinspires.ftc.teamcode.tuning.pedroPathing.Constants;
+
+@Autonomous(name = "blueCloseTwoGate", group = ".")
+public class blueTwoGateAuto extends LinearOpMode {
+
+    private Follower follower;
+
+    RobotHardware bot;
+    DriveSubsystem drivetrain = new DriveSubsystem();
+    LauncherSubsystem launcher = new LauncherSubsystem();
+    TransferSubsystem transfer = new TransferSubsystem();
+    IntakeSubsystem intake = new IntakeSubsystem();
+
+
+    private double shootingVal = autoHalfShootingVelocity;
+
+    @Override
+    public void runOpMode(){
+        bot = new RobotHardware(hardwareMap, new ElapsedTime());
+
+
+        drivetrain.setMotorsMode(bot.driveMotors, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        drivetrain.setMotorsMode(bot.Intake, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        drivetrain.setMotorsMode(bot.Transfer, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        drivetrain.setMotorsMode(bot.flyWheels, DcMotor.RunMode.RUN_USING_ENCODER);
+
+        drivetrain.setZeroPowerBehavior(bot.driveMotors, DcMotor.ZeroPowerBehavior.BRAKE);
+        drivetrain.setZeroPowerBehavior(bot.Intake, DcMotor.ZeroPowerBehavior.BRAKE);
+        drivetrain.setZeroPowerBehavior(bot.Transfer, DcMotor.ZeroPowerBehavior.FLOAT);
+
+        transfer.setGateState(bot.Gate, TransferSubsystem.GateState.CLOSED);
+        transfer.setTransferLevel(bot.Transfer,0);
+        intake.setIntakeLevel(bot.Intake,0);
+        launcher.setFlywheelsVelocity(bot.flyWheels, 0);
+
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(23.919, 128.233, Math.toRadians(144)));
+
+        blueTwoGateAutoPaths paths = new blueTwoGateAutoPaths(follower);
+
+//        drivetrain.enableBuckReads();
+
+        telemetry.addData("X", 23.919);
+        telemetry.addData("Y", 128.233);
+
+        telemetry.addLine("Blue Auto Ready!");
+        telemetry.update();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        // START
+
+        launcher.setFlywheelsVelocity(bot.flyWheels, shootingVal);
+        idleAll();
+
+        // P1
+        follow(paths.preload);
+        sleep(1000); // waiting for flywheels PID
+        releaseALl();
+
+        // C1
+        intakeAll();
+        follow(paths.line1);
+        idleAll();
+        follow(paths.gateOpen1);
+        follow(paths.shoot1);
+        releaseALl();
+
+        // C2
+        idleAll();
+        follow(paths.align2);
+        intakeAll();
+        follow(paths.line2);
+        idleAll();
+        follow(paths.gateOpen2);
+        follow(paths.shoot2);
+        releaseALl();
+
+        // C3
+        idleAll();
+        follow(paths.align3);
+        intakeAll();
+        follow(paths.line3);
+        idleAll();
+        follow(paths.shoot3);
+        releaseALl();
+
+        offAll();
+        follow(paths.leave);
+
+    }
+
+    private void offAll() {
+        transfer.setGateState(bot.Gate, TransferSubsystem.GateState.CLOSED);
+        intake.setIntakeLevel(bot.Intake, 0);
+        transfer.setTransferLevel(bot.Transfer, 0);
+
+    }
+
+
+    private void idleAll() {
+        transfer.setGateState(bot.Gate, TransferSubsystem.GateState.CLOSED);
+        intake.setIntakeLevel(bot.Intake, 1);
+        transfer.setTransferLevel(bot.Transfer, 1);
+    }
+
+    public void intakeAll() {
+        transfer.setGateState(bot.Gate, TransferSubsystem.GateState.CLOSED);
+        intake.setIntakeLevel(bot.Intake, 2);
+        transfer.setTransferLevel(bot.Transfer, 2);
+    }
+
+    private void releaseALl (){
+//        sleep(200);
+//        launcher.setFlywheelsVelocity(bot.flyWheels, shootingVal);
+        intake.setIntakeLevel(bot.Intake, 2);
+        transfer.setTransferLevel(bot.Transfer, 2);
+        sleep(50);
+        transfer.setGateState(bot.Gate, TransferSubsystem.GateState.OPEN);
+        sleep(allReleaseTimer);
+    }
+
+    private void follow(com.pedropathing.paths.PathChain path) {
+        follower.followPath(path);
+
+        while (opModeIsActive() && follower.isBusy()) {
+            follower.update();
+
+            telemetry.addData("Following", path);
+            telemetry.addData("X", follower.getPose().getX());
+            telemetry.addData("Y", follower.getPose().getY());
+            telemetry.addData("Heading", Math.toDegrees(follower.getPose().getHeading()));
+
+            telemetry.addData("\nFL: ", bot.leftFrontDrive.getPower());
+            telemetry.addData("BL: ", bot.leftBackDrive.getPower());
+            telemetry.addData("FR: ", bot.rightFrontDrive.getPower());
+            telemetry.addData("BR: ", bot.rightBackDrive.getPower());
+
+            telemetry.update();
+        }
+
+        follower.breakFollowing();
+        sleep(50);
+    }
+
+    private void follow(com.pedropathing.paths.PathChain path, double power) {
+        follower.followPath(path, power, true);
+
+        while (opModeIsActive() && follower.isBusy()) {
+            follower.update();
+
+            telemetry.addData("Following", path);
+            telemetry.addData("X", follower.getPose().getX());
+            telemetry.addData("Y", follower.getPose().getY());
+            telemetry.addData("Heading", Math.toDegrees(follower.getPose().getHeading()));
+
+            telemetry.addData("\nFL: ", bot.leftFrontDrive.getPower());
+            telemetry.addData("BL: ", bot.leftBackDrive.getPower());
+            telemetry.addData("FR: ", bot.rightFrontDrive.getPower());
+            telemetry.addData("BR: ", bot.rightBackDrive.getPower());
+
+            telemetry.update();
+        }
+
+        follower.breakFollowing();
+        sleep(50);
+    }
+
+}
